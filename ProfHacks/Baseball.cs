6c89b4b17ad8d7eee2395e34abcc91f42f7c8191
Nicholas Weintraut <weintraun8@students.rowan.edu>
2016-03-12 18:48:16 -0500

    Initial Commit
    placed our files and such in here

---
+++ ProfHacks/Baseball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baseball : MonoBehaviour {
+    public GameObject target;
+    Rigidbody rigidComponent;
+    public float force;
+	// Use this for initialization
+	void Start () {
+        rigidComponent = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.rigidComponent.AddForce((target.transform.position - this.transform.position).normalized * force);
+	}
+}
---
+++ ProfHacks/Pitcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pitcher : MonoBehaviour {
+
+    public GameObject spawnBall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            Instantiate(spawnBall);
+        }
+	}
+}
