345183247a31325224364e6ac3bd14081d256ec4
Nicholas Weintraut <weintraun8@students.rowan.edu>
2016-03-13 07:46:01 -0400

    Updated Menu Scripts

---
+++ ProfHacks/Scripts/TriggerButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerButton : MonoBehaviour {
+
+    public GameObject target;
+    public string functionName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter(Collider collider)
+    {
+        target.SendMessage(functionName);
+    }
+}
