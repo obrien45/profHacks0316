83385210b195d21b87353c2c077110a53576460f
Nicholas Weintraut <weintraun8@students.rowan.edu>
2016-03-13 09:57:49 -0400

    added fireball support, needs bugfixing

---
+++ ProfHacks/Scripts/FireballController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class FireballController : MonoBehaviour {
+
+    public GameObject gestureManager;
+    private Controller controller;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<ParticleSystem>().Pause();
+        controller = new Controller();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ParticleSystem fire = this.GetComponent<ParticleSystem>();
+        if (gestureManager.GetComponent<GestureManager>().getCurrentGesture().Equals("Flamethrower"))
+        {
+            if(!fire.isPlaying)
+            {
+                fire.Play();
+            }
+            foreach(Hand hand in controller.Frame().Hands)
+            {
+                this.transform.position = controller.Frame().Hands[0].PalmPosition.ToVector3();
+                this.transform.rotation = Quaternion.LookRotation(controller.Frame().Hands[0].PalmPosition.ToVector3(), Vector3.up);
+            }
+        }
+        else if(!fire.isPaused)
+        {
+            fire.Pause();
+        }
+	}
+}
