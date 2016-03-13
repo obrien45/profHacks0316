d72e15d3a4c4fce8f82db0d8a4a3c43e63d376d0
Paul Bayruns <bayrunsp9@students.rowan.edu>
2016-03-13 11:47:30 -0400

    Added display2
    -Jatin

--- ProfHacks/Scripts/GestureDisplay2.cs
+++ ProfHacks/Scripts/GestureDisplay2.cs
@@ -4,19 +4,20 @@ using UnityEngine.UI;
 
 public class GestureDisplay2 : MonoBehaviour
 {
-
+    public GestureConcat gestConcat;
     public GameObject gestureManager;
 
     // Use this for initialization
     void Start()
     {
-
+        gestConcat = GetComponent<GestureConcat>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Text>().text = gestureManager.GetComponent<GestureManager>().getCurrentGesture();
+        string result = gestConcat.result;
+        this.GetComponent<Text>().text = result; 
     }
 }
 
