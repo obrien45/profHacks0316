f146919438bf5aad291990b5ebdf6d7857357e47
Nicholas Weintraut <weintraun8@students.rowan.edu>
2016-03-13 00:10:19 -0500

    Created some scripts for gesture recognition

---
+++ ProfHacks/Scripts/GestureManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+
+public class GestureManager : MonoBehaviour {
+
+    Dictionary<string, Frame> gestures;
+    //maps name of each gesture (english word) to the gesture (ASL sign)
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void activate()
+    {
+
+    }
+
+    //add a gesture to the saved gestures, with the given name
+    public void addGesture(string name, Frame frame)
+    {
+        gestures.Add(name, frame);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ ProfHacks/Scripts/GestureRecognizer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+
+public class GestureRecognizer : MonoBehaviour {
+    GestureManager manager;
+    Controller controller;
+
+    public List<string> gestureNames;
+
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+	}
+
+    public string getNextGestureName()
+    {
+        return gestureNames[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gestureNames.Count > 0)
+        {
+            if (Input.GetKeyDown("F"))
+            {
+                manager.addGesture(gestureNames[0], controller.Frame());
+                //add this frame to the saved gesture list
+                gestureNames.RemoveAt(0);
+            }
+        }
+        else
+        {
+            manager.activate();
+            Destroy(this);
+        }
+	}
+}
